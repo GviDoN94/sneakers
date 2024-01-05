36b8f89b3953a146a551280311cdd33941ef7260
Vladislav Surin <vlad_surin@mail.ru>
2024-01-06 00:44:46 +0300

    The Header component has created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<template></template>
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+  import Header from '@/components/Header.vue';
+</script>
 
 <style scoped></style>
