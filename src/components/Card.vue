131b525046d8a24e16112e32d0b2b61763d87e73
Vladislav Surin <vlad_surin@mail.ru>
2024-01-06 22:10:34 +0300

    Content for cards is transmitted through props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,26 +3,40 @@
     class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
     <img
+      @click="onClickFavorite"
       class="absolute top-8 left-8"
-      src="/like-1.svg"
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
       alt="Like 1"
     />
     <img
-      src="/sneakers/sneakers-1.jpg"
+      :src="imageUrl"
       alt="Sneaker"
     />
-    <p class="mt-2">Мужские кроссовки Nike Blazer Mid Suede</p>
+    <p class="mt-2">{{ title }}</p>
 
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-200">Цена:</span>
-        <b>1205 руб.</b>
+        <b>{{ price }} руб.</b>
       </div>
 
       <img
-        src="/plus.svg"
+        @click="onClickAdd"
+        :src="!isAdded ? '/plus.svg' : '/checked.svg'"
         alt="Plus"
       />
     </div>
   </div>
 </template>
+
+<script setup>
+  defineProps({
+    title: String,
+    imageUrl: String,
+    price: Number,
+    isFavorite: Boolean,
+    isAdded: Boolean,
+    onClickAdd: Function,
+    onClickFavorite: Function,
+  });
+</script>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isFavorite="true"
+      :onClickAdd="onClickAdd"
+    />
   </div>
 </template>
 
 <script setup>
   import Card from '@/components/Card.vue';
+
+  const onClickAdd = () => alert('Добавить!');
 </script>
